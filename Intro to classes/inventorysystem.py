c8be668b6aa11eddda5aada5e014f06a864c9762
Aiden <a.neizer2007@gmail.com>
2026-01-14 21:19:28 +0000

    Update inventorysystem.py

--- Intro to classes/inventorysystem.py
+++ Intro to classes/inventorysystem.py
@@ -1,13 +1,12 @@
 import json
 
-
 def int_errorhandling(prompt):
     while True:
         try:
             return int(input(prompt))
         except ValueError:
             print("Use Number")
-class inventorysystem:
+class InventorySystem:
     def __init__(self):
         self.items_list = []
 # Reads the exisitng file
@@ -20,30 +19,18 @@ class inventorysystem:
         except FileNotFoundError, json.JSONDecodeError:
             print("You have no existing data.")
     def addinventory(self):
-        while True:
-            item_input = input("Enter a item you'll like to add:")
-            if item_input == int(item_input):
-                print("Invalid Item")
-            else:
-                print("")
-                break
-        price_input = int_errorhandling(f"Enter the cost of {item_input}: £")
-        quantity_input = int_errorhandling(f"Enter a quantity of {item_input}: ")
+        item = input("Enter a item you'll like to add:")
+        price = int_errorhandling(f"Enter the cost of {item}: £")
+        quantity = int_errorhandling(f"Enter a quantity of {item}: ")
         print("====================================")
         print("          ITEM CATEGORIES")
         print("====================================")
         print("  [1] Consumable")
-        print("      - Used up when used")
         print("  [2] Equipment")
-        print("      - Reusable items")
         print("  [3] Material")
-        print("      - Used for crafting or repair")
         print("  [4] Media")
-        print("      - Books, movies, software")
         print("  [5] Clothing")
-        print("      - Wearable items")
         print("  [6] Miscellaneous")
-        print("      - Everything else")
         print("====================================")
 
         category_input = int(input("Enter the category (1-4): "))
@@ -70,11 +57,18 @@ class inventorysystem:
             else:
                 print("Invalid Category. Try Again.")
         
-    datainputted = ItemStuff(item,price,quantity,category)
+        datainputted = ItemStuff(item,price,quantity,category)
         
-    data_dic = {
-        "item: ":item
-    }
+        data_dic = {
+            "Item: ":datainputted.item,
+            "Price: ":datainputted.price,
+            "Quantity: ":datainputted.quantity,
+            "Category: ":datainputted.category,
+            
+        }
+        self.item_list.append(data_dic)
+        with open ("inventoryfile.json", "w") as f:
+                json.dump(self.book)
             
 class ItemStuff:
     def __init__(self,item,price,quantity,category):
@@ -84,7 +78,8 @@ class ItemStuff:
         self.category = category
             
         
-        
+inventorysystem = InventorySystem()
+inventorysystem.addinventory()
 
 
 
