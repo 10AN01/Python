737a331053dbba936f0135d37bf7b43ba6e86de5
Aiden <a.neizer2007@gmail.com>
2026-01-27 21:15:13 +0000

    Added Grades

--- Introduction to MYSQL/Student-Course-Grade-System/studentfunction.py
+++ Introduction to MYSQL/Student-Course-Grade-System/studentfunction.py
@@ -76,6 +76,106 @@ class StudentSystem:
                                             """, (studentID,firstname,lastname,studentemail))
 # conn.commit makes the action permanentS
                         self.conn.commit()
+        def remove_student(self):
+                print("="* 25)
+                print("     ALL STUDENT")
+                print("="* 25)
+# Select everything in the database (Can see everything)
+                self.cursor.execute("SELECT * FROM student")
+                all_student = self.cursor.fetchall()
+# loops depending on how much data there is
+                for student in all_student:
+                        print(f"Student ID: {student[1]}\nFirst Name: {student[2]}\nLast Name: {student[3]} \nEmail: {student[4]}")
+                        print("-"*24)
+# Will get only one data that is selected.
+                studentID_Remove = input("Enter the student ID to remove: ")
+                student = self.cursor.fetchone()
+# If the student ID is found then delete
+                if student is not None:
+                        self.cursor.execute("DELETE FROM student WHERE studentID = ?",(studentID_Remove,))
+                        self.conn.commit()
+                        print("Student removed successfully!")
+                else:
+                        print("No student found with that ID")
+                        return
+        def add_grades(self):
+                print("ADD GRADE")
+# Select everything in the database (Can see everything)
+                self.cursor.execute("SELECT * FROM student")
+                all_student = self.cursor.fetchall()
+# loops depending on how much data there is
+                for student in all_student:
+                        print(f"Student ID: {student[1]}\nFirst Name: {student[2]}\nLast Name: {student[3]} \nEmail: {student[4]}")
+                        print("-"*24)
+                Student_ID = input("Enter the students ID: ")
+                self.cursor.execute("""
+                                    SELECT * FROM student WHERE studentID = ?""",(Student_ID,)
+                                    )
+                
+                student = self.cursor.fetchone()
+                if student is not None:
+                        while True:
+                                grade = int_errorhandling("Enter the student's grade: ")
+                                if grade >= 0 and grade <=9:
+                                        break
+                                else:
+                                        print("Invalid grade. Needs to between from 1 to 9[")
+                        while True:
+                                print("Subjects")
+                                print("1) Computer Science")
+                                print("2) Math")
+                                print("3) English")
+                                print("4) Science")
+                                print("5) History")
+                                print("6) Art")
+                                print("7) Physical Education")
+                                print("8) Music")
+                                print("9) Geography")
+                                print("10) Social Studies")
+                                choice = int_errorhandling("Enter the subject number: ")
+                                if choice == 1:
+                                        subject = "Computer Science"
+                                        break
+                                elif choice == 2:
+                                        subject = "Math"
+                                        break
+                                elif choice == 3:
+                                        subject = "English"
+                                        break
+                                elif choice == 4:
+                                        subject = "Science"
+                                        break
+                                elif choice == 5:
+                                        subject = "History"
+                                        break
+                                elif choice == 6:
+                                        subject = "Art"
+                                        break
+                                elif choice == 7:
+                                        subject = "Physical Education"
+                                        break
+                                elif choice == 8:
+                                        subject = "Music"
+                                        break
+                                elif choice == 9:
+                                        subject = "Geography"
+                                        break
+                                elif choice == 10:
+                                        subject = "Social Studies"
+                                        break
+                                else:
+                                        print("Invalid choice")
+
+                else:
+                        print("Student not found.")
+                        return
+                self.cursor.execute("""
+                                    INSERT INTO grades (studentIdIncrement,studentID,grades,subject)
+                                    VALUES (NULL,?,?,?)
+                                    """, (Student_ID,grade,subject))
+                self.conn.commit()
+
+                        
                         
 class StudentInformation:
         def __init__(self,firstname,lastname,studentID,studentemail):
@@ -85,4 +185,4 @@ class StudentInformation:
                 self.studentemail = studentemail
 
 studentsystem = StudentSystem()
-studentsystem.add_student()
+studentsystem.add_grades()
